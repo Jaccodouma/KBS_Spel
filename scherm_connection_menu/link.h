9177baa12211fef92600304dbe3ca5907b04829d
Christiaan8642 <33750830+Christiaan8642@users.noreply.github.com>
2018-12-19 12:48:51 +0100

    hatseflats

--- scherm_connection_menu/link.h
+++ scherm_connection_menu/link.h
@@ -6,22 +6,27 @@
 class link
 {
   public:
-    link(IR *ir, uint8_t master);
-    uint8_t checkForData(void);
-    void UpdatePlayerInfo(uint8_t x, uint8_t y, uint8_t bomb, uint8_t lifes);
+    link(IR *ir, uint8_t master); //constructor
+    
+    uint8_t checkForData(void); //check if there is new player data
+    
+    void UpdatePlayerInfo(uint8_t x, uint8_t y, uint8_t bomb, uint8_t lives); //update the player data for the other defice
+    
+    void updateTimer(void); //update the timer
 
-    uint8_t otherplayer_x, otherplayer_y, otherplayer_bomb, otherplayer_lifes;
-        
+    uint8_t ping();
+
+    uint8_t otherplayer_x, otherplayer_y, otherplayer_bomb, otherplayer_lives; //Public variables to update the "the going to be sent" data
+
   private:
     IR *ir;
     uint8_t master;
+    uint8_t Enableping = 1;
     uint8_t readyForNewData = 1;
-    uint8_t resentCmd[1]={(1<<1)};
+    uint8_t resentCmd[1] = {(1 << 1)};
     uint8_t EnableSending = 1;
-    uint8_t dataIN[MESSAGE_SIZE]={0,0};   // an array to store the received data
-    uint8_t dataOUT[MESSAGE_SIZE]={0,0};
+    uint8_t dataIN[MESSAGE_SIZE] = {0, 0}; // an array to store the received data
+    uint8_t dataOUT[MESSAGE_SIZE] = {0, 0};
 };
-
-
 
 #endif
--- scherm_dimmer_menu/settingMenu.h
+++ scherm_dimmer_menu/settingMenu.h
@@ -1,26 +1,31 @@
 #ifndef settingMenu_H
 #define settingMenu_H
 
-class settingMenu
+#include "Adafruit_ILI9341.h"	// TFT screen
+#include "Adafruit_STMPE610.h"	// Touch screen
+
+#include "touchScreen.h"
+#include "TaskManager.h"
+
+class SettingMenu: public Task
 {
-  public:
-    settingMenu(touchScreen *ts);
-    uint8_t run(void);
-    void screenBrightness(void);
-    
-  private:
+public:
+	SettingMenu(touchScreen *ts);
+	int run(void);
+	void screenBrightness(void);
+	
+private:
+	void PWM_init(void);
+	void adc_init(void);
+	uint8_t screenbrightnessAuto(void);
 
-    void PWM_init(void);
-    void adc_init(void);
-    uint8_t screenbrightnessAuto(void);
+	uint8_t photocellReading;
+	uint8_t brightness = 150;
+	uint8_t brightness_old = 150;
 
-    uint8_t photocellReading;
-    uint8_t brightness = 150;
-    uint8_t brightness_old = 150;
-
-    touchScreen *ts;
-    Adafruit_STMPE610 *touch;
-    Adafruit_ILI9341 *tft;
+	touchScreen *ts;
+	Adafruit_STMPE610 *touch;
+	Adafruit_ILI9341 *tft;
 };
 
 #endif
